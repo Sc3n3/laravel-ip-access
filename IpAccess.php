f9b470d5346c926079873fd235865d17fa0d47cb
Gökay Solman <gokaysolman@hotmail.com>
2017-02-21 15:46:27 +0300

    code beutify

--- IpAccess.php
+++ IpAccess.php
@@ -18,14 +18,10 @@ class IpAccess
     public function handle($request, Closure $next, ...$ips)
     {
         $access = array_filter(array_map(function($v){
-            return ($star = strpos($v, "*")) ? (substr(getenv('REMOTE_ADDR'), 0, $star) == substr($v, 0, $star))
-                                             : (getenv('REMOTE_ADDR') == $v);
+            return ( $star = strpos($v, "*") ) ? ( substr(getenv('REMOTE_ADDR'), 0, $star) == substr($v, 0, $star) )
+                                               : ( getenv('REMOTE_ADDR') == $v );
         }, $ips));
 
-        if( !$access ) {
-            return App::abort(403);
-        }
-
-        return $next($request);
+        return $access ? $next($request) : App::abort(403);
     }
 }
